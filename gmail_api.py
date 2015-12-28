72cf4d91499ddcca192e59d9ca1ec45f7b889f9f
davidvg <davidvazquezgijon@gmail.com>
2015-12-28 23:39:46 +0100

    Modification in the authentication file that is stored in ~/.credentials.
    Conflicts:
    	test.py

--- gmail_api.py
+++ gmail_api.py
@@ -47,7 +47,7 @@ def get_credentials():
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
     credential_path = os.path.join(credential_dir,
-                                   'gmail-python-quickstart.json')
+                                   'gmail_api-python.json')
 
     store = oauth2client.file.Storage(credential_path)
     credentials = store.get()
